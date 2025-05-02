5c883a76aecb91b0c5114a690f2a3ed95309574a
dev-debabrata <debabratadas711@gmail.com>
2025-05-03 00:45:01 +0530

    03-05-2025

--- Mordern-operators-strings/destructuring-arrays.js
+++ Mordern-operators-strings/destructuring-arrays.js
@@ -3,9 +3,13 @@
 const restaurant = {
     name: 'Indian Food',
     location: 'Kolkata, Egra, Digha, Kharagpur, Belda, Contai',
-    categories: ['Vegetarian, Nonvegetarian, Fast-foods, Chinis Italian, Organic'],
-    starterMenu: ['Momo, Salad, Garlic Bread'],
-    mainMenu: ['Vegetable, Nonvegetable, Pizza, Pasta, Beriyani'],
+    categories: ['Vegetarian', 'Nonvegetarian', 'Fast-foods', 'Chinis Italian', 'Organic'],
+    starterMenu: ['Momo', 'Salad', 'Garlic Bread'],
+    mainMenu: ['Pizza', 'Pasta', 'Beriyani'],
+
+    order: function (starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    },
 };
 
 const arr = [2, 3, 4];
@@ -14,10 +18,10 @@ const b = arr[1];
 const c = arr[2];
 
 const [x, y, z] = arr;
-console.log(x, y, z);
-console.log(arr);
+// console.log(x, y, z);
+// console.log(arr);
 
-let [main, secondary] = restaurant.categories;
+let [main, , secondary] = restaurant.categories;
 console.log(main, secondary);
 
 
@@ -29,3 +33,17 @@ console.log(main, secondary);
 
 [main, secondary] = [secondary, main];
 console.log(main, secondary);
+
+// Receive to reture values from a function
+const [starter, mainMenuAll] = restaurant.order(2, 0);
+console.log(starter, mainMenuAll);
+
+// Nested destructuring 
+const nested = [2, 3, [5, 6]];
+// const [i, , j] = nested;
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+// Default values
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
