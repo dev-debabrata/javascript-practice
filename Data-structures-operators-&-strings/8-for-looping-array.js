a36eb2509d10eb97ab394ed451aaa3cb6ae4923f
dev-debabrata <debabratadas711@gmail.com>
2025-05-05 00:28:22 +0530

    05-05-2025

---
+++ Data-structures-operators-&-strings/8-for-looping-array.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const restaurant = {
+    name: 'Indian Food',
+    location: 'Kolkata, Egra, Digha, Kharagpur, Belda, Contai',
+    categories: ['Vegetarian', 'Nonvegetarian', 'Fast-foods', 'Chinis Italian', 'Organic'],
+    starterMenu: ['Momo', 'Salad', 'Garlic Bread'],
+    mainMenu: ['Pizza', 'Pasta', 'Beriyani'],
+    openingHours: {
+        thu: {
+            open: 10,
+            close: 22,
+        },
+        fri: {
+            open: 12,
+            close: 24,
+        },
+        sat: {
+            open: 0,
+            close: 25,
+        },
+    },
+
+    order: function (starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    },
+
+    orderDelivary: function ({
+        starterIndex = 1,
+        mainIndex = 0,
+        time = '20:00',
+        address,
+    }) {
+        console.log(`Order recevie! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+    },
+
+    orderPasta: function (ing1, ing2, ing3) {
+        console.log(`Here is your declicious pasta with ${ing1}, ${ing2}, ${ing3}`);
+    },
+
+    orderPizza: function (mainIngredient, ...otherIngredients) {
+        console.log(mainIngredient);
+        console.log(otherIngredients);
+    },
+};
+
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+for (const item of menu) console.log(item);
+
+for (const [i, el] of menu.entries()) {
+    console.log(`${i + 1}: ${el}`);
+}
---
+++ Data-structures-operators-&-strings/9-enhanced-object-literals.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+const openingHours = {
+    [weekdays[3]]: {
+        open: 10,
+        close: 22,
+    },
+    [weekdays[4]]: {
+        open: 12,
+        close: 24,
+    },
+    [weekdays[5]]: {
+        open: 0,
+        close: 25,
+    },
+};
+
+const restaurant = {
+    name: 'Indian Food',
+    location: 'Kolkata, Egra, Digha, Kharagpur, Belda, Contai',
+    categories: ['Vegetarian', 'Nonvegetarian', 'Fast-foods', 'Chinis Italian', 'Organic'],
+    starterMenu: ['Momo', 'Salad', 'Garlic Bread'],
+    mainMenu: ['Pizza', 'Pasta', 'Beriyani'],
+    // es6 enhenced object literals
+    openingHours,
+
+    order(starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    },
+
+    orderDelivary({
+        starterIndex = 1,
+        mainIndex = 0,
+        time = '20:00',
+        address,
+    }) {
+        console.log(`Order recevie! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+    },
+
+    orderPasta(ing1, ing2, ing3) {
+        console.log(`Here is your declicious pasta with ${ing1}, ${ing2}, ${ing3}`);
+    },
+
+    orderPizza(mainIngredient, ...otherIngredients) {
+        console.log(mainIngredient);
+        console.log(otherIngredients);
+    },
+};
