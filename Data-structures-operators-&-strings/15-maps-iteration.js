8a691eff026e96c66b9c78cbed609370504c7c30
dev-debabrata <debabratadas711@gmail.com>
2025-05-07 23:59:35 +0530

    07-05-2025

---
+++ Data-structures-operators-&-strings/15-maps-iteration.js
@@ -0,0 +1,81 @@
+'use strict';
+
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+const openingHours = {
+    [weekdays[3]]: {
+        open: 10,
+        close: 22,
+    },
+    [weekdays[4]]: {
+        open: 12,
+        close: 24,
+    },
+    [weekdays[5]]: {
+        open: 0,
+        close: 25,
+    },
+};
+
+const restaurant = {
+    name: 'Indian Food',
+    location: 'Kolkata, Egra, Digha, Kharagpur, Belda, Contai',
+    categories: ['Vegetarian', 'Nonvegetarian', 'Fast-foods', 'Chinis Italian', 'Organic'],
+    starterMenu: ['Momo', 'Salad', 'Garlic Bread'],
+    mainMenu: ['Pizza', 'Pasta', 'Beriyani'],
+    // es6 enhenced object literals
+    openingHours,
+
+    order(starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    },
+
+    orderDelivary({
+        starterIndex = 1,
+        mainIndex = 0,
+        time = '20:00',
+        address,
+    }) {
+        console.log(`Order recevie! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+    },
+
+    orderPasta(ing1, ing2, ing3) {
+        console.log(`Here is your declicious pasta with ${ing1}, ${ing2}, ${ing3}`);
+    },
+
+    orderPizza(mainIngredient, ...otherIngredients) {
+        console.log(mainIngredient);
+        console.log(otherIngredients);
+    },
+};
+
+const question = new Map([
+    ['question', 'What is the best programming language in the world'],
+    [1, 'C'],
+    [2, 'Java'],
+    [3, 'JavaScript'],
+    [true, 'Correct'],
+    [false, 'Try again'],
+]);
+console.log(question);
+
+// Convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+// Quiz app
+console.log(question.get('question'));
+for (const [key, value] of question) {
+    if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+
+const answer = 3;
+console.log(answer);
+
+console.log(question.get(question.get('correct') === answer));
+
+// Convert map to array
+console.log([...question]);
+console.log([...question.key()]);
+console.log([...question.values()]);
