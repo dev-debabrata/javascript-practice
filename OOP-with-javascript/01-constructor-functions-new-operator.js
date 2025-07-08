2c14f3acfde3414204597c260ba9c9ce16a2eb2b
dev-debabrata <debabratadas711@gmail.com>
2025-07-08 23:11:06 +0530

    08-07-2025

---
+++ OOP-with-javascript/01-constructor-functions-new-operator.js
@@ -0,0 +1,29 @@
+const Person = function (firstName, birthYear) {
+    // Instance properties
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+
+    // Never to this!
+    // this.calcAge = function () {
+    //   console.log(2037 - this.birthYear);
+    // };
+};
+
+const debu = new Person('Debu', 1997);
+console.log(debu);
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
+
+const nick = new Person('Nick', 2017);
+const nikhil = new Person('Nikhil', 1975);
+
+console.log(debu instanceof Person);
+
+Person.hey = function () {
+    console.log('Hey there 👋');
+    console.log(this);
+};
+Person.hey();
