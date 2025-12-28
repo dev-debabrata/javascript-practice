3438105589d7b1974f1c803fe2fd490a82becc09
Debabrata Das <debabratadas711@gmail.com>
2025-12-28 20:06:41 +0530

    palindrome

---
+++ 11-palindrome.js
@@ -0,0 +1,15 @@
+function isPalindrome(num) {
+    if (num < 0) return false;
+
+    let original = num;
+    let reversed = 0;
+
+    while (num > 0) {
+        let digit = num % 10;
+        reversed = reversed * 10 + digit;
+        num = Math.floor(num / 10);
+    }
+    return original === reversed;
+}
+
+console.log(isPalindrome(121));
